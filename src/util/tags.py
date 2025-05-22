55e5c5f733576508847e6511d6b43b0131232456
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-05-22 11:26:39 -0600

    create tags.py

---
+++ src/util/tags.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-08 -*-
+
+"""
+Tag dictionaries.
+"""
+
+upos_tags = {
+    'ADJ': 'adjective',
+    'ADP': 'adposition',
+    'ADV': 'adverb',
+    'AUX': 'auxiliary',
+    'CCONJ': 'coordinating conjunction',
+    'DET': 'determiner',
+    'INTJ': 'interjection',
+    'NOUN': 'noun',
+    'NUM': 'numeral',
+    'PART': 'particle',
+    'PRON': 'pronoun',
+    'PROPN': 'proper noun',
+    'PUNCT': 'punctuation',
+    'SCONJ': 'subordinating conjunction',
+    'SYM': 'symbol',
+    'VERB': 'verb',
+    'X': 'other',
+}
