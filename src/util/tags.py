2221ed3858392e6f618e38c2694f5cf5953d4bbe
Carlos A. Lang-Sanou <carlos.lang@gmail.com>
2025-05-23 14:01:51 -0600

    add Grammatical categories

--- src/util/tags.py
+++ src/util/tags.py
@@ -7,6 +7,9 @@ Universal Dependencies tag dictionaries.
 Ref: https://universaldependencies.org/u/pos/
 """
 
+"""
+POS tags from Universal Dependencies.
+"""
 upos_tags = {
     'ADJ': 'adjective',
     'ADP': 'adposition',
@@ -25,4 +28,39 @@ upos_tags = {
     'SYM': 'symbol',
     'VERB': 'verb',
     'X': 'other',
+}
+
+"""
+Grammatical categories that show up in the constituency parse tree.
+"""
+gram_categories = {
+    'ROOT',
+    'S',
+    'conj',
+    'gerundi',
+    'grup.a',
+    'grup.adv',
+    'grup.cc',
+    'grup.cs',
+    'grup.nom',
+    'grup.prep',
+    'grup.pron',
+    'grup.verb',
+    'grup.w',
+    'grup.z',
+    'inc',
+    'infinitiu',
+    'interjeccio',
+    'morfema.pronominal',
+    'morfema.verbal',
+    'neg',
+    'participi',
+    'prep',
+    'relatiu',
+    's.a',
+    'sadv',
+    'sentence',
+    'sn',
+    'sp',
+    'spec',
 }
